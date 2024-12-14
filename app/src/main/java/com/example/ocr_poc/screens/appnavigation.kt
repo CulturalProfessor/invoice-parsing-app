ab8ab1239b415690cec819c8d569163b8557b62d
himanshiiiiii <himanshitripathi14@gmail.com>
2024-12-14 15:45:47 +0530

    page added

--- app/src/main/java/com/example/ocr_poc/screens/appnavigation.kt
+++ app/src/main/java/com/example/ocr_poc/screens/appnavigation.kt
@@ -11,7 +11,7 @@ import androidx.navigation.compose.rememberNavController
 fun AppNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "scroll_screen") {
+    NavHost(navController = navController, startDestination = "splash") {
         composable("splash") { SplashScreen(navController) }
         composable("main") { MainScreen(navController) }
         composable("scroll_screen") { ScrollScreenWithTabs(  onBackClick = { navController.popBackStack()}) }
